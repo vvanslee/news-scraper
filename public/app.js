b1af1bf612313da9142e7664bc8e589b7ec1aa2d
vvanslee <vvanslee@gmail.com>
2017-09-24 22:07:21 -0500

    things were updated and added to the things

--- public/app.js
+++ public/app.js
@@ -1,26 +1,7 @@
-// function displayResults(scrapedData) {
-//     $("tbody").empty();
-  
-//     scrapedData.forEach(function(scrapedData) {
-//       $("tbody").append("<tr><td>" + scrapedData.title + "</td>" +
-//                               "<td>" + scrapedData.byline + "</td>" +
-//                               "<td>" + scrapedData.link + "</td>" +
-//                               "<td>" + scrapedData.summary + "</td></tr>");
-//     });
-//   }
-  
-//   function setActive(selector) {
-    
-//   }
-  
-//   $.getJSON("/all", function(data) {
-//     // Call our function to generate a table body
-//     displayResults(data);
-//   });
 $(document).ready(function(){
     $.getJSON("/all", function(data) {
         for (var i = 0; i < data.length; i++) {
-        $(".articles").append("<p data-id='" + data[i].title + "'>" + data[i].link + "<br />" + data[i].summary + "</p>");
+        $(".articles").append("<p><h3><a href='" + data[i].link + "' >"  + data[i].title + "</a></h3><br/>" + data[i].byline + "<br/>" + data[i].summary + "</p>");
         }
     });
 });
