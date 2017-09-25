cd96e82d194ce7ad9f62ead9d31574cff580bf43
vvanslee <vvanslee@gmail.com>
2017-09-24 19:32:33 -0500

    i dont even know

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var databaseUrl = "news-scraper";
 var collections = ["scrapedData"];
 
 // Website to be scraped
-var siteURL = "https://techcrunch.com/";
+var siteUrl = "https://techcrunch.com/";
 
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
@@ -20,42 +20,21 @@ db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-
-app.get("/scrape", function (req, res) {
-    request(siteURL, function(err, response, html) {
-      if (err) {
-        console.log(err);
-      } else {
-
-          //oisgoarnglaeng
-          
-    }
-});
-});
-
-
 app.get("/scrape", function (req, res) {
 // Make a request call to grab the HTML body from techcrunch.com
-request(siteURL, function(error, response, html) {
-    
-    // Load the HTML into cheerio and save it to a variable
-    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-    var $ = cheerio.load(html);
-    
-    // Select each element in the HTML body from which you want information.
-    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-    // but be sure to visit the package's npm page to see how it works
-    $("div.block-content").each(function(i, element) {
+    request(siteUrl, function(error, response, html) {
+        
+        // Load the HTML into cheerio and save it to a variable
+        // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+        var $ = cheerio.load(html);
+        
+        $("div.block-content").each(function(i, element) {
 
-        var title = $(element).children("h2.post-title").text();
-        var byline = $(element).children("div.byline").find("a").text();
-        var link = $(element).find("h2.post-title").find("a").attr("href");
-        var summary = $(element).children("p.excerpt").text();
+            var title = $(element).children("h2.post-title").text();
+            var byline = $(element).children("div.byline").find("a").text();
+            var link = $(element).find("h2.post-title").find("a").attr("href");
+            var summary = $(element).children("p.excerpt").text();
 
-        // Save these results in an object that we'll push into the results array we defined earlier
-        //if (err) {
-        //    console.log(err);
-        //} else {
             db.scrapedData.insert({
                 title: title,
                 byline: byline,
@@ -65,11 +44,11 @@ request(siteURL, function(error, response, html) {
                 res.json(result);
             });
 
-        //}
-
+        });
     });
-
-// Log the results once you've looped through each of the elements found with cheerio
-//console.log(results);
 });
+
+// Listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
 });
